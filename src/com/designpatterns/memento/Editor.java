d41a16c5f375c79aa4ceb171c06b3f49ce229f5d
eigenjoe <eigenjosephmendoza@gmail.com>
2022-11-05 16:44:00 -0400

    Implemented the Memento Design Pattern

---
+++ src/com/designpatterns/memento/Editor.java
@@ -0,0 +1,11 @@
+package com.designpatterns.memento;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+public class Editor {
+    private String content = "";
+
+}
---
+++ src/com/designpatterns/memento/EditorState.java
@@ -0,0 +1,2 @@
+package com.designpatterns.memento;public class EditorState {
+}
