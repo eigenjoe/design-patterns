57706d7700892e9fef369f28b8e145a124c9e85c
eigenjoe <eigenjosephmendoza@gmail.com>
2022-11-06 10:53:28 -0500

    Implemented the template method pattern.

---
+++ src/com/designpatterns/templateMethod/AuditTrail.java
@@ -0,0 +1,8 @@
+package com.designpatterns.templateMethod;
+
+public class AuditTrail {
+
+    public void record() {
+        System.out.println("Recording...");
+    }
+}
---
+++ src/com/designpatterns/templateMethod/Task.java
@@ -0,0 +1,16 @@
+package com.designpatterns.templateMethod;
+
+public abstract class Task {
+    private AuditTrail auditTrail;
+
+    public Task(AuditTrail auditTrail) {
+        this.auditTrail = auditTrail;
+    }
+
+    public void execute() {
+        auditTrail.record();
+        doExecute();
+    }
+
+    protected abstract void doExecute();
+}
