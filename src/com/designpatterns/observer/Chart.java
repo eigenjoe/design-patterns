e8b7f49186048a7adc8c85f22bed5250a9ee628d
eigenjoe <eigenjosephmendoza@gmail.com>
2022-11-08 16:35:48 -0500

    Implemented the Observer pattern

---
+++ src/com/designpatterns/observer/Chart.java
@@ -0,0 +1,13 @@
+package com.designpatterns.observer;
+
+public class Chart implements Observer {
+    private DataSource dataSource;
+
+    public Chart(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+    @Override
+    public void update() {
+        System.out.println("Chart got notified" + dataSource.getValue());
+    }
+}
