2cd1ca860cbe79beb016a508263d1d5a5146ad8a
eigenjoe <eigenjosephmendoza@gmail.com>
2022-11-08 19:07:26 -0500

    Implemented classic Mediator design pattern

---
+++ src/com/designpatterns/mediator/ListBox.java
@@ -0,0 +1,18 @@
+package com.designpatterns.mediator;
+
+public class ListBox extends UIControl {
+    public ListBox(DialogBox owner) {
+        super(owner);
+    }
+
+    private String selection;
+
+    public String getSelection() {
+        return selection;
+    }
+
+    public void setSelection(String selection) {
+        this.selection = selection;
+        owner.changed(this);
+    }
+}
---
+++ src/com/designpatterns/mediator/UIControl.java
@@ -0,0 +1,11 @@
+package com.designpatterns.mediator;
+
+public class UIControl  {
+    protected DialogBox owner;
+
+    public UIControl(DialogBox owner) {
+        this.owner = owner;
+    }
+
+
+}
